7ba4e15cd2fc2c2d0061e5f7a4ad49247e0d079c
VolodinAlekse <volodin.8989@mail.ru>
2023-02-06 02:30:35 +0300

    demo_018_string

--- demo_018/Program.cs
+++ demo_018/Program.cs
@@ -5,23 +5,26 @@
 System.Console.WriteLine("Введите номер четверти: ");
 int q = Convert.ToInt32(Console.ReadLine());
 
+string[] array = { "x > 0, y > 0", "x < 0 , y > 0", "x < 0, y < 0", "x > 0, y < 0", "Такой четверти нет" };
+
+
 if (q == 1)
 {
-    System.Console.WriteLine(" x > 0, y > 0");
+    System.Console.WriteLine(array[0]);
 }
 else if (q == 2)
 {
-    System.Console.WriteLine(" x < 0, y > 0");
+    System.Console.WriteLine(array[1]);
 }
 else if (q == 3)
 {
-    System.Console.WriteLine(" x < 0 && y < 0");
+    System.Console.WriteLine(array[2]);
 }
 else if (q == 4)
 {
-    System.Console.WriteLine(" x > 0 && y < 0");
+    System.Console.WriteLine(array[3]);
 }
 else
 {
-    System.Console.WriteLine("Начало координат");
+    System.Console.WriteLine(array[4]);
 }
