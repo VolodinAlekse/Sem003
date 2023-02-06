1321a548a6f2642db7312105d70183598f744a73
VolodinAlekse <volodin.8989@mail.ru>
2023-02-06 03:29:02 +0300

    demo_021_022

--- demo_021_022/Program.cs
+++ demo_021_022/Program.cs
@@ -7,16 +7,40 @@
 //Math.Pow(Число, количество квадратов)
 //Math.Sqrt(Квадратный корень из указанного числа)
 
-int InputNumber(string str)
+// int InputNumber(string str)
+// {
+//     System.Console.WriteLine(str);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int x1 = InputNumber("Введите координату x1: ");
+// int y1 = InputNumber("Введите координату y1: ");
+
+// int x2 = InputNumber("Введите координату x2: ");
+// int y2 = InputNumber("Введите координату y2: ");
+
+// System.Console.WriteLine(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)));
+
+
+// Напишите программу, 
+// которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+System.Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// int count = 1;
+
+// while (count <= N)
+// {
+//     System.Console.Write(Math.Pow(count, 2) + " ");
+//     count++;
+// }
+
+for(int count = 1; count <= N; count++)
 {
-    System.Console.WriteLine(str);
-    return Convert.ToInt32(Console.ReadLine());
+    System.Console.Write(Math.Pow(count, 2) + " ");
 }
-
-int x1 = InputNumber("Введите координату x1: ");
-int y1 = InputNumber("Введите координату y1: ");
-
-int x2 = InputNumber("Введите координату x2: ");
-int y2 = InputNumber("Введите координату y2: ");
-
-System.Console.WriteLine(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)));
